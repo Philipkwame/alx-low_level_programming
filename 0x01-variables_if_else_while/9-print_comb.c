566cf56c869a27930662e1f2f62fb9f40c57153d
philipkwame <mrphilipacquah@gmail.com>
2023-08-12 08:41:10 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int num;
+	char i;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
+
