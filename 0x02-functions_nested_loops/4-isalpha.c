8583ed639117805456b6582dc9fae0dceb36aa83
philipkwame <mrphilipacquah@gmail.com>
2023-08-15 17:47:01 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for loweercase character
+ * _isalpha - checks for loweercase character
  * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
