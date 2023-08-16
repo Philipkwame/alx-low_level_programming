118195be67f646d048e619e159485c37a645ee4e
philipkwame <mrphilipacquah@gmail.com>
2023-08-16 02:39:00 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * jack_bauer = prints every minute of the day of jack Bauer
+ * jack_bauer - Prints every minute of the day of jack Bauer
  * Return: ...
  */
 
 void jack_bauer(void)
 
 {
-	int a,b,c,d;
+	int a , b , c , d;
 
 	for (a = 0; a <= 2; a++)
 	{
