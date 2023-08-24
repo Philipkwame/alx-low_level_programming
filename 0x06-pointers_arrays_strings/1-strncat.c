43a0de09d3c20b361ebd258635b711a601d141a1
philipkwame <mrphilipacquah@gmail.com>
2023-08-23 18:29:43 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	int len = strlen(dest);
 	int i;
 
-	for(i = 0 ; i< n && *src != '\0' ; i++)
+	for (i = 0 ; i < n && *src != '\0' ; i++)
 	{
 		dest[len + i] = src[i];
 		src++;
