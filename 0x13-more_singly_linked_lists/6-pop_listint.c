6029d475689f91a41117e7c8251aedc89802a2bd
Philipkwame <mrphilipacquah@gmail.com>
2023-09-26 05:56:06 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,17 +10,17 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int alpha;
+	int num;
 
 	if (!head || !*head)
 		return (0);
 
-	alpha = (*head)->n;
+	num = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
 	*head = temp;
 
-	return (alhpa);
+	return (num);
 }
 
 
